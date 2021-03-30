8fec12e8fcf5aae1e417364978b4e8dc8118bba8
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-03-30 15:37:42 -0500

    Replaced the data inside the home.js file and used the fetch mathoed inside the json data file to carry over the data and display this on the screen for the user.

--- src/Home.js
+++ src/Home.js
@@ -2,11 +2,7 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My New Website', body: 'lorem ipsum...', author: 'Natalia', id: 1},
-        { title: 'Welcome Home', body: 'lorem ipsum...', author: 'Briana', id: 2},
-        { title: 'Dev Top Tips', body: 'lorem ipsum...', author: 'Vladimir', id: 3},
-    ]);
+    const [blogs, setBlogs] = useState(null);
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -14,12 +10,18 @@ const Home = () => {
     }
 
     useEffect(() => {
-        console.log('use effect ran')
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data)
+            })
     }, [])
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title='All Blogs!' handleDelete={handleDelete} />
+            {blogs && <BlogList blogs={blogs} title='All Blogs!' handleDelete={handleDelete} />}
         </div>
     );
 }
