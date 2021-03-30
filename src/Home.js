efce0f7be814f158c221a4d6e81afd87a1ecd90e
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-03-30 14:00:12 -0500

    Added the 3 blogs with title, body, and author and used the map methoed with the use state hook to have them cycled through and displayed on the hpme page.

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,20 @@
-const Home = () => {
+import { useState } from 'react';
 
-    const handleClick = () => {
-        console.log()
-    }
+const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'Natalia', id: 1},
+        { title: 'Welcome home', body: 'lorem ipsum...', author: 'Briana', id: 2},
+        { title: 'Dev top tips', body: 'lorem ipsum...', author: 'Vladimir', id: 3},
+    ]);
 
     return (
         <div className="home">
-            <h2>Home Page</h2>
-            <button onClick={handleClick}>Click Me</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
         </div>
     );
 }
