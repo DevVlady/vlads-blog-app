c99c124ac3d1f1b1ae809ad5d78d564e9ca15784
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-03-30 15:40:50 -0500

    Removed the handle deleted function.

--- src/Home.js
+++ src/Home.js
@@ -4,11 +4,6 @@ import BlogList from './BlogList';
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs)
-    }
-
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
             .then(res => {
@@ -21,7 +16,7 @@ const Home = () => {
 
     return (
         <div className="home">
-            {blogs && <BlogList blogs={blogs} title='All Blogs!' handleDelete={handleDelete} />}
+            {blogs && <BlogList blogs={blogs} title='All Blogs!' />}
         </div>
     );
 }
