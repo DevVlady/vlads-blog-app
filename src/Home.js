3a311fc09aeab4f26b7fdb3078389c15f13bb011
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-03-30 13:24:49 -0500

    Created the handle click event inside the home.js file and passed this function to the button under the home page title.

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,13 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log()
+    }
+
     return (
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={handleClick}>Click Me</button>
         </div>
     );
 }
