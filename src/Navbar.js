8984149fff2bc21696e0e5b91b085cca4b3e9085
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-03-30 12:55:51 -0500

    Created the navbar with 2 href's which will be two routes that the user will be routed to when they click on Home or New Blog buttons.

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,6 +2,10 @@ const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>Vladimir's Blog</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
         </nav>
     );
 }
