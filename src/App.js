2a61bdbccd8dc1e3e0feacdf5dd556c9e4bf9aa9
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-03-30 12:53:37 -0500

    Created the navbar.js file which will be a componenet used to create the navbar.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 
 function App() {
-  const title = 'Welcome to Vlads Blog';
 
   return (
     <div className="App">
       <div className="content">
-        <h1>{title}</h1>
       </div>
     </div>
   );
---
+++ src/Navbar.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>Vladimir's Blog</h1>
+        </nav>
+    );
+}
+
+export default Navbar;
