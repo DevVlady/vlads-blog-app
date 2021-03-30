9d8e39c696ccecfaa6dfe615fb2fba7185f932ce
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-03-30 18:57:11 -0500

    Inisde the create.js file I added the form that will be displayed allowing the user to create a new blog post.

--- src/Create.js
+++ src/Create.js
@@ -2,6 +2,24 @@ const Create = () => {
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form>
+                <label>Blog Title:</label>
+                <input
+                    type="text"
+                    required
+                />
+                <label>Blog body:</label>
+                <textarea
+                    type="text"
+                    required
+                ></textarea>
+                <label>Blog Author:</label>
+                <select>
+                    <option value="Vladimir">Vladimir</option>
+                    <option value="Briana">Briana</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
     );
 }
