2ce810991b15ec2c6541e39dd1837bf9a6507607
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-03-30 13:01:44 -0500

    Imported the home.js file in the app,js and nested this inside the return statement.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Navbar />
       <div className="content">
+        <Home />
       </div>
     </div>
   );
