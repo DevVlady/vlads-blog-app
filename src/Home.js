c315820114fff92771732827645fefa480e4cdf1
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-03-30 15:59:12 -0500

    Added the error message and states to catch the errors if any and display those errors to the user on the webpage.

--- src/Home.js
+++ src/Home.js
@@ -3,21 +3,31 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true)
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
             .then(res => {
+                if(!res.ok) {
+                    throw Error('Could not fetch the data');
+                }
                 return res.json();
             })
             .then(data => {
                 setBlogs(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch(err => {
+                setError(err.message);
                 setIsPending(false);
             })
     }, [])
 
     return (
         <div className="home">
+            {error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title='All Blogs!' />}
         </div>
