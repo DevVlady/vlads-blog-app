5cd39674c842d5a37460b8d43080ef3bbcad4c47
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-03-30 15:49:17 -0500

    Added the loading to be displayed when the data is being fetchwed from the json file.

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true)
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
@@ -10,12 +11,14 @@ const Home = () => {
                 return res.json();
             })
             .then(data => {
-                setBlogs(data)
+                setBlogs(data);
+                setIsPending(false);
             })
     }, [])
 
     return (
         <div className="home">
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title='All Blogs!' />}
         </div>
     );
