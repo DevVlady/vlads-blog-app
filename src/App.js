b1c1d3eac5898ea67ee1c2e5be90699986515b08
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-03-30 12:57:59 -0500

    Imported the navbar component inside the app.js file.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
       </div>
     </div>
