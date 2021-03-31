970d65607ddbb2ad31c04946df242fe353d512ce
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-03-31 13:23:07 -0500

    Styled the 404 page to look a little more presentable.

--- src/NotFoundPg.js
+++ src/NotFoundPg.js
@@ -4,7 +4,7 @@ const NotFoundPage = () => {
     return (
         <div className="not-found">
             <h2>We Apologize...</h2>
-            <p>The page you are looking for cannot be found</p>
+            <p>The page you are looking for cannot be found!</p>
             <p>Click on the link below to be routed back to the main page</p>
             <Link to='/'>Main Page...</Link>
         </div>
