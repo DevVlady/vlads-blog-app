240aa787072d65902cadf98db46951c217b5d719
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-03-31 13:13:51 -0500

    Created a 404 page with a redirect link for when the user reaches a page that is not found in the application.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFoundPage from './NotFoundPg';
 
 function App() {
 
@@ -20,6 +21,9 @@ function App() {
             </Route>
             <Route exact path="/blogs/:id">
               <BlogDetails />
+            </Route>
+            <Route path='*'>
+              <NotFoundPage />
             </Route>
           </Switch>
         </div>
---
+++ src/NotFoundPg.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+const NotFoundPage = () => {
+    return (
+        <div className="not-found">
+            <h2>We Apologize...</h2>
+            <p>The page you are looking for cannot be found</p>
+            <p>Click on the link below to be routed back to the main page</p>
+            <Link to='/'>Main Page...</Link>
+        </div>
+    );
+}
+
+export default NotFoundPage;
