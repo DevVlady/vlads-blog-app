daec32828d66b60bf0bc1ba214c11e18a82230f6
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-03-31 12:23:30 -0500

    Added the fetch request so that when the user clicks on the add new blog, this new blog gets posted to the db.json file.

--- src/Create.js
+++ src/Create.js
@@ -8,6 +8,14 @@ const Create = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
         const blog = {title, body, author};
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log('new blog added')
+        })
     }
 
     return (
