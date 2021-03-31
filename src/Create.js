cb8e127b9cda3199e016cbd6953c594577142554
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-03-30 19:16:48 -0500

    Aded the usestate for all fields of the add blog form so that this can be tracked.

--- src/Create.js
+++ src/Create.js
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Vladimir');
+
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
@@ -7,18 +13,28 @@ const Create = () => {
                 <input
                     type="text"
                     required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
                 />
                 <label>Blog body:</label>
                 <textarea
                     type="text"
                     required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
                 ></textarea>
                 <label>Blog Author:</label>
-                <select>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
                     <option value="Vladimir">Vladimir</option>
                     <option value="Briana">Briana</option>
                 </select>
                 <button>Add Blog</button>
+                <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p>
             </form>
         </div>
     );
